1bfb11dfc453a40d14525caab5c96ac4e26f8f34
twil3akine <twil3akine@gmail.com>
2026-07-20 01:03:46 +0900

    chore(rust): main.rsを初期化

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-use atcoder::geometry::{segments_intersect, Point};
-use atcoder::io::Writer;
+use atcoder::{input, io::*};
 
 fn main() {
-    let mut writer = Writer::new();
+    let mut wr = Writer::new();
 
-    atcoder::input! {
-        t: usize,
-    }
-
-    let mut answers = Vec::with_capacity(t);
-    for _ in 0..t {
-        atcoder::input! {
-            p: (isize, isize),
-            q: (isize, isize),
-            r: (isize, isize),
-            s: (isize, isize),
-        }
-        answers.push(usize::from(segments_intersect(
-            Point::new(p.0, p.1),
-            Point::new(q.0, q.1),
-            Point::new(r.0, r.1),
-            Point::new(s.0, s.1),
-        )));
-    }
-
-    writer.join_line(answers);
+    input! {
+        
+    };
 }
